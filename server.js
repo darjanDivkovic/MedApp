da121de97f5f08b0847108ade1146b870915b8ad
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-29 11:14:32 +0100

    Set up mustache view engine, rendered meds.mustache in a simple /meds rout

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
-let express = require('express');
+const express = require('express');
+const mustacheExpress = require('mustache-express');
 
-let app = express();
+const app = express();
+const mustache = mustacheExpress();
+mustache.cache = null;
+app.engine('mustache', mustache);
+app.set('view engine', 'mustache');
+
 app.use(express.static('public'));
+
+app.get('/meds', (req,res)=> {
+    res.render('meds');
+});
 
 app.listen(1502, ()=> {
     console.log('Listening to port 1502.');
