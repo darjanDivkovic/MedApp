cd65119518da06cf44f45dcd61ac13c40bf383fd
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-30 21:08:54 +0100

    Installed body parser and set it up

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const mustache = mustacheExpress();
@@ -12,6 +13,14 @@ app.use(express.static('public'));
 app.get('/meds', (req,res)=> {
     res.render('meds');
 });
+
+app.get('/add', (req,res)=>{
+    res.render('med-form');
+});
+
+app.post('/meds/add', (req,res)=>{
+    res.redirect('/meds');
+})
 
 app.listen(1502, ()=> {
     console.log('Listening to port 1502.');
