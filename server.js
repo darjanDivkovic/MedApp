6ee5d6e16ac62cffb0072d5d829333f64ab003ba
Darjan Divkovic <daarjan.tz@gmail.com>
2020-01-30 21:30:19 +0100

    Everythin connected, sending data from forms to DB!

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const { Client } = require('pg');
 
 const app = express();
 const mustache = mustacheExpress();
@@ -9,6 +10,7 @@ app.engine('mustache', mustache);
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/meds', (req,res)=> {
     res.render('meds');
@@ -19,7 +21,29 @@ app.get('/add', (req,res)=>{
 });
 
 app.post('/meds/add', (req,res)=>{
+    console.log('post body', req.body);
     res.redirect('/meds');
+
+    const client = new Client({
+
+        user: 'postgres',
+        host: 'localhost',
+        database: 'medical',
+        password: 'darjan1234',
+        port: 5432
+
+    });
+
+    client.connect()
+        .then(()=> {
+            console.log('We are in boys!');
+            const query = 'INSERT INTO meds(name,count) VALUES($1, $2)';
+            const params = [req.body.name, req.body.count];
+            return client.query(query,params);
+        })
+        .then((result)=> {
+            res.redirect('/meds');
+        });
 })
 
 app.listen(1502, ()=> {
